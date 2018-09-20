41193c5e87fdfaea2b660bcb87120fd6bf650aba
RemainTheSame <34090420+RemainTheSame@users.noreply.github.com>
2018-09-20 13:14:07 +0200

    BikeTest version 2

--- app/src/androidTest/java/tda367/paybike/model/BikeTest.java
+++ app/src/androidTest/java/tda367/paybike/model/BikeTest.java
@@ -7,16 +7,6 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.util.Date;
-
 import static org.junit.Assert.*;
 
     /**
