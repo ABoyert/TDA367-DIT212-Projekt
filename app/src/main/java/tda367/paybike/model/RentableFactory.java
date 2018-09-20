d7e24a226ebb28672ee76dbd466d6ac00c4cba85
RemainTheSame <34090420+RemainTheSame@users.noreply.github.com>
2018-09-20 14:49:26 +0200

    BikeTest version 4

---
+++ app/src/main/java/tda367/paybike/model/RentableFactory.java
@@ -0,0 +1,22 @@
+package tda367.paybike.model;
+
+/**
+ * Created by Oscar Orava Kilberg on 2018-09-20.
+ */
+
+public class RentableFactory {
+
+    int i;
+
+    public static Rentable createRentable(String type, String id, double price, String[] position){
+
+
+        if("Bike".equalsIgnoreCase(type))return new Bike(id, price, position );
+
+
+        return null;
+
+
+    }
+
+}
