8a7da592a8c3d56454e7833efe130aa098a3150e
RemainTheSame <34090420+RemainTheSame@users.noreply.github.com>
2018-09-20 08:57:12 +0200

    Bike and Rentable basic methods and attributes implemented

--- app/src/main/java/tda367/paybike/model/Bike.java
+++ app/src/main/java/tda367/paybike/model/Bike.java
@@ -1,8 +1,71 @@
 package tda367.paybike.model;
 
+import java.util.Date;
+
 /**
  * Created by Oscar Orava Kilberg on 2018-09-19.
  */
 
 public class Bike implements Rentable {
+
+    private Date[] rentDate;
+    private String id;
+    private double price;
+    private String[] position;
+    private boolean available;
+
+    @Override
+    public boolean isAvailable() {
+        return available;
+    }
+
+    @Override
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void setPrice(Double price) {
+        this.price = price;
+
+    }
+
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+
+    @Override
+    public void setPosition(String pos[]) {
+        this.position = pos;
+    }
+
+    @Override
+    public String[] getPosition() {
+        return position;
+    }
+
+    @Override
+    public void setDate(Date start, Date end) {
+        this.rentDate = new Date[]{start, end};
+    }
+
+    @Override
+    public Date[] getRentingDate() {
+        return rentDate;
+    }
+
+
 }
--- app/src/main/java/tda367/paybike/model/Rentable.java
+++ app/src/main/java/tda367/paybike/model/Rentable.java
@@ -1,8 +1,25 @@
 package tda367.paybike.model;
 
+import java.util.Date;
+
 /**
  * Created by Oscar Orava Kilberg on 2018-09-19.
  */
 
 public interface Rentable {
+
+    public void setId(String id);
+    public String getId();
+    public void setPrice(Double price);
+    public double getPrice();
+    public void setPosition(String pos[]);
+    public String[] getPosition();
+    public void setDate(Date start, Date end);
+    public Date[] getRentingDate();
+    public void setAvailable(boolean available);
+    public boolean isAvailable();
+
+
+
+
 }
