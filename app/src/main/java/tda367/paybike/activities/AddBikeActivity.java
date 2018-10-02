f901f6f67abffba7469a3d3ced837b90278a106f
Unknown <gustafsson.julia@live.se>
2018-10-02 14:12:19 +0200

    Add constraints for input fields in AddBikeActivity

--- app/src/main/java/tda367/paybike/activities/AddBikeActivity.java
+++ app/src/main/java/tda367/paybike/activities/AddBikeActivity.java
@@ -1,12 +1,13 @@
 package tda367.paybike.activities;
 
-import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import tda367.paybike.R;
 import tda367.paybike.model.Bike;
@@ -14,17 +15,9 @@ import tda367.paybike.viewmodels.AddBikeViewModel;
 
 public class AddBikeActivity extends AppCompatActivity {
 
-    private static final int MAX_PRICE = 1000;
-    private static final int MAX_DESCRIPTION_LENGTH = 1000;
-    private static final int MAX_TITLE_LENGTH = 50;
-
-    private static final String INVALID_NAME = "Bike title cannot exceed " + MAX_TITLE_LENGTH + " characters.";
-    private static final String INVALID_POSITION = "Invalid address format.";
-    private static final String INVALID_DESCRIPTION = "Description cannot exceed " + MAX_DESCRIPTION_LENGTH + " characters.";
-    private static final String INVALID_PRICE = "Price cannot exceed $" + MAX_PRICE;
-
     private Button addBikeBtn;
     private EditText bikeName, bikeDescription, bikePosition, bikePrice;
+    private TextView warning;
 
     private AddBikeViewModel model;
 
@@ -36,58 +29,104 @@ public class AddBikeActivity extends AppCompatActivity {
         model = ViewModelProviders.of(this).get(AddBikeViewModel.class);
 
         addBikeBtn = (Button) findViewById(R.id.postBikeBtn);
+        addBikeBtn.setFocusable(true);
+
         bikeName = (EditText) findViewById(R.id.nameText);
+        bikeName.setText(model.getBikeName());
+        bikeName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus == false) {
+                    model.setBikeName(bikeName.getText().toString());
+                }
+            }
+        });
+
         bikeDescription = (EditText) findViewById(R.id.descriptionText);
+        bikeDescription.setText(model.getBikeDescription());
+        bikeDescription.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus == false) {
+                    model.setBikeDescription(bikeDescription.getText().toString());
+                }
+            }
+        });
+
+        // TODO Check if this needs to be updated when we have a position object to store addresses
         bikePosition = (EditText) findViewById(R.id.positionText);
+        bikePosition.setText(model.getBikeDescription());
+        bikePosition.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus == false) {
+                    model.setBikePosition(bikePosition.getText().toString());
+                }
+            }
+        });
+
         bikePrice = (EditText) findViewById(R.id.priceText);
+        bikePrice.setText(model.getBikePrice());
+        bikePrice.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus == false) {
+                    model.setBikePrice(bikePrice.getText().toString());
+                }
+            }
+        });
+
+        warning = (TextView) findViewById(R.id.inputInvalidText);
 
         addBikeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                postBike();
+                updateModel();
+                if (model.inputIsValid()) {
+                    model.postBike();
+                    clearAllTextFields();
+                    if (model.warningIsShown()) {
+                        hideWarning();
+                    }
+                    finish(); // End Activity
+                } else {
+                    showWarning();
+                }
             }
         });
-
     }
 
-    private boolean inputIsValid() {
-
-        return false;
+    private void updateModel() {
+        model.setBikeName(bikeName.getText().toString());
+        model.setBikeDescription(bikeDescription.getText().toString());
+        model.setBikePosition(bikePosition.getText().toString());
+        model.setBikePrice(bikePrice.getText().toString());
     }
 
-    private void addBike() {
-
+    private void clearAllTextFields() {
+        bikeName.getText().clear();
+        model.setBikeName("");
+        bikeDescription.getText().clear();
+        model.setBikeDescription("");
+        bikePosition.getText().clear();
+        model.setBikePosition("");
+        bikePrice.getText().clear();
+        model.setBikePrice("");
     }
 
-    private boolean nameLengthIsValid() {
-
-        return bikeName.getText().length() < 50? true : false;
+    // Fetches warning message from ViewModel and shows it in the GUI
+    private void showWarning() {
+        warning.setText(model.getWarning());
+        warning.setVisibility(View.VISIBLE);
+        // Notify ViewModel of change
+        model.showWarning(true);
     }
 
-    private boolean priceIsValid() {
-        return bikePrice.getText().length() < 4 ? true : false;
+    // Hides warning message in the GUI
+    private void hideWarning() {
+        warning.setVisibility(View.GONE);
+        warning.setText("");
+        // Notify ViewModel of change
+        model.showWarning(false);
     }
-
-    private boolean descriptionIsValid() {
-        return bikeDescription.getText().length() < 1000 ? true : false;
-    }
-
-    private boolean positionIsValid() {
-        //TODO Write method to check this
-        return false;
-    }
-
-    private void postBike() {
-        Bike newBike = new Bike(bikeName.getText().toString(),
-                Double.parseDouble(bikePrice.getText().toString()),
-                bikePosition.getText().toString(),
-                true,
-                "owner",
-                "imageLink",
-                bikeDescription.getText().toString());
-
-        model.postBike(newBike);
-    }
-
-
 }
