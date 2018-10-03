771f659e41499ede31681492727433ec1afd5057
RemainTheSame <halooscur.orava@gmail.com>
2018-10-03 12:52:42 +0200

    Bike replaced by abstraction Rentable, step 2.1

--- app/src/main/java/tda367/paybike/model/RentableFactory.java
+++ app/src/main/java/tda367/paybike/model/RentableFactory.java
@@ -2,13 +2,26 @@ package tda367.paybike.model;
 
 /**
  * Created by Oscar Orava Kilberg on 2018-09-20.
+ *
  */
 
 public class RentableFactory {
 
-    int i;
+    int j;
 
-    public static Rentable createRentable(String type, String name, double price, String position, boolean available, String owner, String imageLink, String description){
+    public static Rentable createRentable(String type, String name, double price, String position, boolean available, String owner, String imageLink, String description, String id){
+
+
+        if("Bike".equalsIgnoreCase(type))
+            return new Bike(name, price, position, available, owner, imageLink, description, id);
+
+
+        return null;
+
+
+    }
+
+    public static Rentable createRentableNoID(String type, String name, double price, String position, boolean available, String owner, String imageLink, String description){
 
 
         if("Bike".equalsIgnoreCase(type))
@@ -19,5 +32,6 @@ public class RentableFactory {
 
 
     }
+
 
 }
