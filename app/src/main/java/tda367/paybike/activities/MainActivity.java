fea882b1b20333aa4ddb9c4e0d36abe5acf14980
Pontus Backman <ponbac@student.chalmers.se>
2018-10-07 14:55:07 +0200

    added code to test createUser in MainActivity

--- app/src/main/java/tda367/paybike/activities/MainActivity.java
+++ app/src/main/java/tda367/paybike/activities/MainActivity.java
@@ -47,13 +47,6 @@ public class MainActivity extends AppCompatActivity implements
         // DO NOT DELETE
         DatabaseController db = DatabaseController.getInstance();
 
-        // Open sign-in page
-        //createSignInIntent();
-
-        // EXAMPLE USE
-        //UserHandler uhtest = UserHandler.getInstance();
-        //uhtest.signIn("USERNAME", "PASSWORD");
-
         findBikeBtn.setOnClickListener(v -> {
             UserHandler uh = UserHandler.getInstance();
             MainViewModel mvm = new MainViewModel();
@@ -75,11 +68,6 @@ public class MainActivity extends AppCompatActivity implements
         registerNewUser.setOnClickListener(view -> registerNewUser(view));
     }
 
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-    }
-
     private void registerNewUser(View view) {
         RegisterUserFragment newUser = RegisterUserFragment.newInstance();
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -90,23 +78,8 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onFragmentInteraction(Uri uri) {
 
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                Log.d(TAG, "Current user: " + user.getDisplayName());
-                // ...
-            } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
-            }
-        }
     }
+
 }
