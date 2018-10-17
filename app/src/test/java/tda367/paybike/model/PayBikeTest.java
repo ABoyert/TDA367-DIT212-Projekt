56477933a85f5894a3c72ad34dca3768189628b7
RemainTheSame <34090420+RemainTheSame@users.noreply.github.com>
2018-10-17 15:34:18 +0200

    RequestTest in PayBike fix

--- app/src/test/java/tda367/paybike/model/PayBikeTest.java
+++ app/src/test/java/tda367/paybike/model/PayBikeTest.java
@@ -13,12 +13,13 @@ public class PayBikeTest {
     @Test
     public void addRequest(){
         PayBike payBike = PayBike.getInstance();
+        PayBike.setCurrentUser(new User("test@test.com", "password", "Testare", "0"));
         User currentUser = payBike.getCurrentUser();
         User targetUser = new User("test@t.com", "password", "Testare", "1");
         Uri image = Uri.parse("Image");
         Rentable testRentable = RentableFactory.createRentableNoID("Bike", "Cykeltest", 25.00, "Testland", true, targetUser.getUserID(), image, "En test");
         payBike.addRequest(currentUser, testRentable, LocalDateTime.of(2020, 12, 24, 11,0), LocalDateTime.of(2020, 12, 24, 12, 0), 10);
-        assert !payBike.getModelRequests().isEmpty();
+        assertFalse (payBike.getModelRequests().isEmpty());
     }
 
 }
